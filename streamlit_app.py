56c74dc09dd9b06dc4e21f3d99f2724bc5db9201
pavel3489 <162901642+pavel3489@users.noreply.github.com>
2025-01-14 11:13:26 +0000

    sync

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎮 Gamification")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+
+    ""
 )
